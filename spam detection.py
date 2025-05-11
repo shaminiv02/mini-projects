6d28a819009b770491a036c8ac2d6a98c9fe294e
Shamini <162007196+shaminiv02@users.noreply.github.com>
2025-05-11 17:38:07 +0530

    Add files via upload

---
+++ spam detection.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+url = "https://raw.githubusercontent.com/justmarkham/pycon-2016-tutorial/master/data/sms.tsv"
+df = pd.read_csv(url, sep='\t', header=None, names=['label', 'message'])
+df['label'] = df['label'].map({'ham': 0, 'spam': 1})
+X = df['message']
+y = df['label']
+vectorizer = CountVectorizer()
+X_vectorized = vectorizer.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X_vectorized, y, test_size=0.2, random_state=42)
+model = MultinomialNB()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+
+# In[ ]:
+
+
+
+
